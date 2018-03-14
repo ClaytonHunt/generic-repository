6fedf6580dfff8dadb10d793059856339bcf6e94
Clayton Hunt <claytonhunt_104@hotmail.com>
2018-03-14 10:49:06 -0400

    added migrations

---
+++ ContosoUniversity/Migrations/20180314135635_InitialCreate.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+
+namespace ContosoUniversity.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                "Course",
+                table => new
+                {
+                    CourseId = table.Column<int>(),
+                    Credits = table.Column<int>(),
+                    Title = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Course", x => x.CourseId);
+                });
+
+            migrationBuilder.CreateTable(
+                "Student",
+                table => new
+                {
+                    ID = table.Column<int>()
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    EnrollmentDate = table.Column<DateTime>(),
+                    FirstMidName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Student", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                "Enrollment",
+                table => new
+                {
+                    EnrollmentID = table.Column<int>()
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CourseID = table.Column<int>(),
+                    Grade = table.Column<int>(nullable: true),
+                    StudentID = table.Column<int>()
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Enrollment", x => x.EnrollmentID);
+                    table.ForeignKey(
+                        "FK_Enrollment_Course_CourseID",
+                        x => x.CourseID,
+                        principalTable: "Course",
+                        principalColumn: "CourseId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        "FK_Enrollment_Student_StudentID",
+                        x => x.StudentID,
+                        principalTable: "Student",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                "IX_Enrollment_CourseID",
+                "Enrollment",
+                column: "CourseID");
+
+            migrationBuilder.CreateIndex(
+                "IX_Enrollment_StudentID",
+                "Enrollment",
+                column: "StudentID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                "Enrollment");
+
+            migrationBuilder.DropTable(
+                "Course");
+
+            migrationBuilder.DropTable(
+                "Student");
+        }
+    }
+}
---
+++ ContosoUniversity/PaginatedList.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContosoUniversity
+{
+    public class PaginatedList<T> : List<T>
+    {
+        public int PageIndex { get; }
+        public int TotalPages { get; }
+        public bool HasPreviousPage => PageIndex > 1;
+        public bool HasNextPage => PageIndex < TotalPages;
+
+        private PaginatedList(IEnumerable<T> items, int count, int pageIndex, int pageSize)
+        {
+            PageIndex = pageIndex;
+            TotalPages = (int) Math.Ceiling(count / (double) pageSize);
+
+            AddRange(items);
+        }
+
+        public static async Task<PaginatedList<T>> CreateAsync(IQueryable<T> source, int pageIndex, int pageSize)
+        {
+            var count = await source.CountAsync();
+            var items = await source.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToListAsync();
+
+            return new PaginatedList<T>(items, count, pageIndex, pageSize);
+        }
+    }
+}
